6d434d2c62c1c4f298c8ff9e0bfb58c55058ba70
soham-joshi <smjoshi1506@gmail.com>
2022-04-14 23:41:15 +0530

    Maven Project created!

---
+++ src/main/java/devops_scientific_calc/Application.java
@@ -0,0 +1,12 @@
+package devops_scientific_calc;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
